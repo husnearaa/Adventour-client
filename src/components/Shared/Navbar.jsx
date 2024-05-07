47ef2d771b9483c34050c5504961500ea315ea88
husneara <husnearaa99@gmail.com>
2024-05-07 14:21:02 +0600

    has added navbar

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,9 +1,64 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
         <div>
-            
+              <div>
+            <nav className="flex justify-between items-center flex-col gap-3 md:flex-row py-5 px-6 text-base shadow-xl ">
+                <h2 className="text-4xl font-extrabold dark:text-white">Adven<span className="text-[#DD991B]">tour</span></h2>
+                <ul className="flex gap-5 flex-col md:flex-row font-semibold text-xl dark:text-white">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#DD991B] " : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/rooms"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#DD991B] " : ""
+                            }
+                        >
+                            Tourist Places
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/bookings"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#DD991B] " : ""
+                            }
+                        >
+                            Places list
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/gallery"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#DD991B]" : ""
+                            }
+                        >
+                            Add tourist place
+                        </NavLink>
+                    </li>
+                </ul>
+                <button className="btn border-[#DD991B] text-black bg-white w-18 border-2  rounded-full">Login</button>
+                {/* <div className="">
+                        <button>
+                            <div className="w-10 rounded-full border-2 border-red-500">
+                                <img src="" alt="" />
+                            </div>
+                        </button>
+                    </div> */}
+            </nav>
+        </div>
         </div>
     );
 };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
-import Footer from "../components/Shared/Footer";
+// import Footer from "../components/Shared/Footer";
 import Nav1 from "../components/Shared/Nav1";
+import Navbar from "../components/Shared/Navbar";
 
 
 
@@ -10,7 +11,8 @@ const MainLayout = () => {
         <div className="max-w-full mx-auto ">
             {/* <h1 className="text-3xl text-red-700">This is layout</h1> */}
             <Nav1></Nav1>
-            <Footer></Footer>
+            <Navbar></Navbar>
+            {/* <Footer></Footer> */}
         </div>
     );
 };
